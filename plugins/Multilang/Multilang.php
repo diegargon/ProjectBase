7b9d23c48c275d2fe8a9a77b483d4d67aa300b0e
diegargon <diego@envigo.net>
2016-05-17 13:05:25 +0200

    fix multilag

--- plugins/Multilang/Multilang.php
+++ plugins/Multilang/Multilang.php
@@ -12,20 +12,24 @@ function Multilang_init(){
     $config['multilang'] = 1;
 
     $request_uri = $_SERVER['REQUEST_URI'];
-    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);   
-    if(!isset($lang)) {
-        $lang = $config['WEB_LANG'];
-    }
 
-    if (isset($_GET['lang'])) {
-        $lang = s_char($_GET['lang'], 2);
-        $config['WEB_URL'] = $config['WEB_URL'] . "$lang/";
-    } else if ($_SERVER['REQUEST_URI'] == '/') {
-        $request_uri = $lang . $request_uri ;
-        $request_uri = $config['WEB_URL'] . $request_uri;        
+    if (
+       (isset($_GET['lang'])) &&
+       (($lang = s_char($_GET['lang'], 2)) != false)
+        ) {
+            $config['WEB_URL'] = $config['WEB_URL'] . "$lang/";
+            $config['WEB_LANG'] = $lang;
+    } else {
+        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);   
+        if(isset($lang)) {
+            $config['WEB_LANG'] = $lang;
+        }
+    }
+    
+    if ($request_uri == '/') {
+        $request_uri = $config['WEB_URL'] . $config['WEB_LANG'] . $request_uri;        
         header('Location:' .$request_uri);
     }
-    $config['WEB_LANG'] = $lang;
    
     register_action("add_nav_element", "ML_nav", 6);
     
