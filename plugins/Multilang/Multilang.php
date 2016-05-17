f89bdb1ad2b5b8ac8794b0b7352de7d8087b3ccd
diegargon <kn@envigo.net>
2016-05-17 14:36:01 +0200

    do change lang nav works

--- plugins/Multilang/Multilang.php
+++ plugins/Multilang/Multilang.php
@@ -30,18 +30,41 @@ function Multilang_init(){
         $request_uri = $config['WEB_URL'] . $config['WEB_LANG'] . $request_uri;        
         header('Location:' .$request_uri);
     }
-   
+
+    if( //TODO: check lang agains pb_lang
+            isset($_POST['choose_lang']) &&
+            (($choose_lang = s_char($_POST['choose_lang'], 3)) != false)
+            ) {
+            $request_uri = str_replace($config['WEB_LANG'], $choose_lang, $request_uri);
+            header('Location:' .$request_uri);
+    }
+
     register_action("add_nav_element", "ML_nav", 6);
-    
+    register_action("add_script", "ML_Script", 5);   
 }
 
+function ML_Script() {
+    //TODO: Plugin for provided common scripts "need_jquery() and asured its included only one time if its called from other modules";
+    //TODO: jquery ATM its duplicated on login.
+    $script = "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js\"></script>\n";
+    $script .= "<script type='text/javascript'>\n"
+            . "jQuery(function() {\n"
+            . "jQuery('#choose_lang').change(function() {\n"
+            . "this.form.submit();\n"
+            . "});\n"
+            . "});\n"
+            . "</script>\n";
+           
+    
+    return $script;
+}
 function ML_nav() {
     //TODO automatic  * better
     global $config;
     
     $mlnav = "<li class='nav_right'>"
     . "<form action='' method='post'>"
-    . "<select>";
+    . "<select name='choose_lang' id='choose_lang'>";
     if ($config['WEB_LANG'] == "es") {
         $mlnav .= "<option selected value='es'>Español</option>";
     } else {
