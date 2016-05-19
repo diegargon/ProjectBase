9766bc9ae46ed361579dc5fba1c77f23786ec7c2
diegargon <kn@envigo.net>
2016-05-19 10:35:27 +0200

    update

--- plugins/SMBasic/lang/en/SMBasic.lang.php
+++ plugins/SMBasic/lang/en/SMBasic.lang.php
@@ -18,5 +18,24 @@ $LANGDATA['L_EMAIL_EXAMPLE'] = "my@email.com";
 $LANGDATA['L_PASSWORD'] = "Password";
 $LANGDATA['L_PASSWORD_EXAMPLE'] = "eg. y8df!99e7";
 $LANGDATA['L_REMEMBERME'] = "Remember me";
+$LANGDATA['L_USERNAME'] = "Username";
+
+
+$LANGDATA['L_ERROR_INTERNAL'] = "Internal error";
+$LANGDATA['L_ERROR_EMAILPASSWORD'] = "Wrong email or password";
+$LANGDATA['L_ERROR_EMAIL'] = "Email incorrect";
+$LANGDATA['L_ERROR_USERNAME'] = "Username contain invalid characters or too long";
+$LANGDATA['L_ERROR_PASSWORD'] = "Password contain invalid characters or too long";
+$LANGDATA['L_ERROR_PASSWORD_MIN'] = "Password too short";
+$LANGDATA['L_ERROR_USERNAME_EXISTS'] = "Username exists";
+$LANGDATA['L_ERROR_EMAIL_EXISTS'] = "Email exists";
+$LANGDATA['L_REG_ERROR_WHILE'] = "Register Error, please try again";
+
+$LANGDATA['L_REGISTER_OKMSG'] = "Register sucessfull";
+$LANGDATA['L_REGISTER_OKMSG_CONFIRMATION'] = "Register sucessfull. We send a confirmation email to your adress";
+
+$LANGDATA['L_REG_EMAIL_SUBJECT'] = "Your registration";
+$LANGDATA['L_REG_EMAIL_MSG_ACTIVE'] = "You acount has been created, to active please click in this link:";
+$LANGDATA['L_REG_EMAIL_MSG_WELCOME'] = "Welcome...";
 
 //$LANGDATA['L_'] = "";
