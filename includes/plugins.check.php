accd9decb7b50d0354a9489ffac8f4d309461c66
diegargon <kn@envigo.net>
2016-05-21 22:48:32 +0200

    Remove br from print debug

--- includes/plugins.check.php
+++ includes/plugins.check.php
@@ -16,10 +16,10 @@ function get_all_enabled_plugins() {
             $jsondata = file_get_contents ("$full_json_filename");
             $plugin_data = json_decode($jsondata);
             if ($plugin_data->enabled){
-                //print_debug("Plugin $plugin_data->plugin_name added to the registered<br/>");
+                //print_debug("Plugin $plugin_data->plugin_name added to the registered");
                 array_push($registered_plugins, $plugin_data);                
             } else {
-                if (DEBUG_PLUGINS_LOAD) { print_debug("Plugin $plugin_data->plugin_name dropped by disable<br/>"); }
+                if (DEBUG_PLUGINS_LOAD) { print_debug("Plugin $plugin_data->plugin_name dropped by disable"); }
             }
         }
     }
@@ -36,29 +36,29 @@ function start_registered_plugins() {
 
     
     foreach ($registered_plugins as $plugin ) {
-        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Checking $plugin->plugin_name ...<br/>"); }
+        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Checking $plugin->plugin_name ..."); }
         if (!$plugin->autostart) {
-            if (DEBUG_PLUGINS_LOAD) { print_debug("Info: No autostart omitted<br/>"); }            
+            if (DEBUG_PLUGINS_LOAD) { print_debug("Info: No autostart omitted"); }            
         }   else {
-            if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Autostart ON checking for start<br/>"); }            
+            if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Autostart ON checking for start"); }            
             if(checker_plugin($plugin)) {                
                 init_plugin($plugin);                
             }
         }
     }
-    if (DEBUG_PLUGINS_LOAD) { print_debug("<br/>Info: Finish starting all register plugins with autostart ON <br/><br/>"); }            
+    if (DEBUG_PLUGINS_LOAD) { print_debug("<br/>Info: Finish starting all register plugins with autostart ON "); }            
 }
 
 function checker_plugin($plugin) {
     if (check_if_already_started($plugin)){
-        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name already started<br/>"); }
+        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name already started"); }
         return false;
     } else {
-        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name not started yet continue checking<br/>"); }
+        if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name not started yet continue checking"); }
     }
     
     if (check_provided_conflicts($plugin)) {
-        if (DEBUG_PLUGINS_LOAD) { print_debug("<b>ERROR:</b> Conflicts $plugin->plugin_name, another plugin provided *$provided*<br/>"); }
+        if (DEBUG_PLUGINS_LOAD) { print_debug("<b>ERROR:</b> Conflicts $plugin->plugin_name, another plugin provided *$provided*"); }
         return false;
     }
     
@@ -77,7 +77,7 @@ function checker_plugin($plugin) {
 function plugin_manual_start($pluginname) {
     global $registered_plugins;
 
-    if(DEBUG_PLUGINS_LOAD) { print_debug("Info: Manual order to start $pluginname<br/>"); }
+    if(DEBUG_PLUGINS_LOAD) { print_debug("Info: Manual order to start $pluginname"); }
     foreach ($registered_plugins as $plugin ) {
         if ($plugin->plugin_name == $pluginname) {
            if(checker_plugin($plugin)) {
@@ -86,7 +86,7 @@ function plugin_manual_start($pluginname) {
            }
         }
     }
-    if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error:</b> Plugin $pluginname not exist<br/>"); }
+    if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error:</b> Plugin $pluginname not exist"); }
     return false;
 }
 
@@ -104,7 +104,7 @@ function check_provided_conflicts ($plugin){
     
     $allprovided = preg_split('/\s+/', $plugin->provided);
     foreach ($allprovided as $provided) {
-        //echo "$plugin->plugin_name ($provided)<br/>";
+        //echo "$plugin->plugin_name ($provided)";
         if (empty($provided)) {
             return false;
         }        
@@ -134,7 +134,7 @@ function init_plugin($plugin) {
     global $started_plugins;
     
 
-    if (DEBUG_PLUGINS_LOAD) { print_debug("Info: All checks OK: Starting $plugin->plugin_name <br/>"); }
+    if (DEBUG_PLUGINS_LOAD) { print_debug("Info: All checks OK: Starting $plugin->plugin_name "); }
     require_once("plugins/$plugin->plugin_name/$plugin->main_file");
     
     
@@ -143,7 +143,7 @@ function init_plugin($plugin) {
         $init_function();
         
     } else {
-        if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error:</b>Function init on $plugin->plugin_name no exist<br>"); }
+        if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error:</b>Function init on $plugin->plugin_name no exist"); }
         return false;
     }
     array_push($started_plugins, $plugin);
@@ -162,11 +162,11 @@ function plugin_resolve_depends($plugin) {
         $result = check_if_depedencie_started($depends);
 
         if (!$result) {
-            if (DEBUG_PLUGINS_LOAD) { print_debug ("Info: Searching for the necessary dependencies... <br>"); }
+            if (DEBUG_PLUGINS_LOAD) { print_debug ("Info: Searching for the necessary dependencies... "); }
             if( find_dependencies_and_start($depends)) {
-                if (DEBUG_PLUGINS_LOAD) { print_debug ("Info: Found/Fill the necesary dependencies and we can starting the plugin <br>"); }
+                if (DEBUG_PLUGINS_LOAD) { print_debug ("Info: Found/Fill the necesary dependencies and we can starting the plugin "); }
             } else {
-                if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error</b> No depedences for this plugin in the registered plugins<br/>"); }
+                if (DEBUG_PLUGINS_LOAD) { print_debug("<b>Error</b> No depedences for this plugin in the registered plugins"); }
                 $meet_deps = false;
             }
         }
@@ -183,12 +183,12 @@ function check_if_depedencie_started($depends) {
         $allprovided = preg_split('/\s+/', $plugin->provided);
         foreach ($allprovided as $provided) {        
             if ($provided == $depends) {
-                if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name ready, has the dependence we need $depends and its already started<br/>"); }
+                if (DEBUG_PLUGINS_LOAD) { print_debug("Info: Plugin $plugin->plugin_name ready, has the dependence we need $depends and its already started"); }
                 return true;
             }
         }
     }
-    if (DEBUG_PLUGINS_LOAD) { print_debug("Info:No plugins already started to solve the dependence we need: $depends<br/>"); }
+    if (DEBUG_PLUGINS_LOAD) { print_debug("Info:No plugins already started to solve the dependence we need: $depends"); }
     return false;
 }
 
@@ -199,7 +199,7 @@ function find_dependencies_and_start($depends) {
         $allprovided = preg_split('/\s+/', $plugin->provided);
         foreach ($allprovided as $provided) {
             if ($provided == $depends){
-                $meet_deps = plugin_resolve_depends($plugin); //We resolv de dependes of the depends
+                $meet_deps = plugin_resolve_depends($plugin); //resolv de dependes of the depends
                 if ($meet_deps) {
                     init_plugin($plugin);
                     return true;
