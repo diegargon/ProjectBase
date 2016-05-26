92f2abe94ee90b58686f3c7a43583cae6cfc198b
diegargon <kn@envigo.net>
2016-05-27 01:07:37 +0200

    add pre/post_action for check with admin other more simple "build web" behaviour

--- plugins/tplBasic/tpl/basic_body.tpl.php
+++ plugins/tplBasic/tpl/basic_body.tpl.php
@@ -6,11 +6,14 @@ global $tpldata;
 ?>
 <body id="Top">
     <?php 
+    isset($tpldata['PRE_ACTION_ADD_TO_BODY']) ? print $tpldata['PRE_ACTION_ADD_TO_BODY'] : false;
+    
     if (empty($tpldata['ADD_TO_BODY'])) {
         echo "<p>Hello World</p>";
     } else {
        echo "$tpldata[ADD_TO_BODY]";
     }
+    isset($tpldata['POST_ACTION_ADD_TO_BODY']) ? print $tpldata['POST_ACTION_ADD_TO_BODY'] : false;
 
 
 
