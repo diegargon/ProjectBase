8603d8dc00aa8ef9dba250536346a82b50c507f0
diegargon <kn@envigo.net>
2016-06-09 20:57:00 +0200

    fix todo

--- plugins/SimpleACL/admin/SimpleACL.admin.php
+++ plugins/SimpleACL/admin/SimpleACL.admin.php
@@ -9,7 +9,7 @@ function SimpleACL_AdminInit() {
 }
 
 function SimpleACL_AdminMenu($params) {
-    print_debug("SimpleACL.admin.php: A way to assign uniq numbers".__LINE__, "TODO");
+    //TODO A way to assign uniq numbers
     $tab_num = 102;
     if ($params['admtab'] == $tab_num) {
         register_uniq_action("admin_get_content", "SimpleACL_AdminContent");        
