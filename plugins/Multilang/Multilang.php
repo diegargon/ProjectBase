e7c19a5a97e2df71f0e08e95fbf7ebe28b72f5de
diegargon <kn@envigo.net>
2016-06-09 02:55:59 +0200

    fix bug, and others irrelevant

--- plugins/Multilang/Multilang.php
+++ plugins/Multilang/Multilang.php
@@ -5,10 +5,9 @@
 if (!defined('IN_WEB')) { exit; }
 
 function Multilang_init(){
-    global $config;
-    global $ml;
+    global $config, $ml;
     
-    if (DEBUG_PLUGINS_LOAD) { print_debug("Multilang Inititated<br/>"); }
+    if ('DEBUG_PLUGINS_LOAD' && 'DEBUG') { print_debug("Multilang Inititated<br/>"); }
 
     includePluginFiles("Multilang");      
     
@@ -16,8 +15,7 @@ function Multilang_init(){
         $ml = new Multilang;
     }      
 
-    register_uniq_action("get_site_langs",  array($ml, "get_site_langs")); //REMOVE
-  
+    register_uniq_action("get_site_langs",  array($ml, "get_site_langs")); //REMOVE  
     $request_uri = $_SERVER['REQUEST_URI']; //TODO FILTER
     
     if (
@@ -31,21 +29,16 @@ function Multilang_init(){
         if(isset($lang)) {
             $config['WEB_LANG'] = $lang;
         }
-    }
-    
+    }   
     if ($request_uri == '/') {
         $request_uri = $config['WEB_URL'] . $config['WEB_LANG'] . $request_uri;        
         header('Location:' .$request_uri);
     }
-
     if( //TODO: check lang agains pb_lang
         isset($_POST['choose_lang']) && (($choose_lang = S_POST_CHAR_AZ("choose_lang", 2, 2)) != false)) {
         $request_uri = str_replace("/". $config['WEB_LANG'], "/".$choose_lang, $request_uri); // FIX better method than can replace something not wanted.
         header('Location:' .$request_uri);
     }
-
-    register_action("nav_element", array($ml, "get_nav"), 6);
-    
-    addto_tplvar("SCRIPTS", $ml->get_js());    
-  
+    register_action("nav_element", array($ml, "get_nav"), 6);   
+    addto_tplvar("SCRIPTS", $ml->get_js());     
 }
--- plugins/Multilang/admin/Multilang.admin.php
+++ plugins/Multilang/admin/Multilang.admin.php
@@ -33,13 +33,13 @@ function Multilang_AdminContent($params) {
         addto_tplvar("ADM_CONTENT_H2", $LANGDATA['L_GENERAL'] .": ".  $LANGDATA['L_PL_STATE'] );                
         addto_tplvar("ADM_CONTENT", Admin_GetPluginState("Multilang"));        
     } else if ($opt == 2) { 
-        if (empty($_POST['btnModifyLang'])) {
+        if (!empty($_POST['btnModifyLang'])) {
             Multilang_ModifyLang();
         }
-        if (empty($_POST['btnCreateLang'])) {
+        if (!empty($_POST['btnCreateLang'])) {
             Multilang_CreateLang();
         }        
-        if (empty($_POST['btnDeleteLang'])) {
+        if (!empty($_POST['btnDeleteLang'])) {
             Multilang_DeleteLang();
         }             
         addto_tplvar("ADM_CONTENT",  Multilang_AdminLangs());          
