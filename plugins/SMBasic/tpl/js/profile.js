d79ef4353f3dec1b6ad1354c9e19a1d364e178b9
diegargon <kn@envigo.net>
2016-06-20 11:14:45 +0200

    disable submit button while form processing

--- plugins/SMBasic/tpl/js/profile.js
+++ plugins/SMBasic/tpl/js/profile.js
@@ -5,6 +5,7 @@
 
 $(document).ready(function(){
     $("#profile").click(function(){
+        $('#profile').attr('disabled','disabled');
         //Email Validation
         var reg = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
         
@@ -55,11 +56,11 @@ $(document).ready(function(){
                 } else if(json[0].status == 5) {
                     $('#email').css("border","2px solid red");
                     $('#email').css("box-shadow","0 0 3px red");                                     
-                    alert(json[0].msg);                    
-                    
+                    alert(json[0].msg);                                        
                 } else {
                     alert(json[0].msg);
                 }
+                $('#profile').removeAttr("disabled");
                 });
             return false;
     });
