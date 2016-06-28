b9fbefe0088fe1b791a55f7211e524ee92dfa2d8
diegargon <kn@envigo.net>
2016-06-28 18:31:29 +0200

    small fix

--- plugins/Newspage/tpl/news_body_style1.tpl.php
+++ plugins/Newspage/tpl/news_body_style1.tpl.php
@@ -9,13 +9,13 @@ if (!defined('IN_WEB')) { exit; }
             
              <?php isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false ?> 
             <div id="col1">
-               <?php   isset($tpldata['COL1_ARTICLES']) ? $tpldata['COL1_ARTICLES'] : false ?>
+               <?php   isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false ?>
             </div>
             <div id="col2">
-               <?php   isset($tpldata['COL2_ARTICLES']) ? $tpldata['COL2_ARTICLES'] :false ?>
+               <?php   isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] :false ?>
             </div>
             <div id="col3">
-               <?php   isset($tpldata['COL3_ARTICLES']) ? $tpldata['COL1_ARTICLES'] : false ?>
+               <?php   isset($tpldata['COL3_ARTICLES']) ? print $tpldata['COL3_ARTICLES'] : false ?>
             </div>            
             <?php isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS']:false ?>            
         </div>
