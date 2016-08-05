4717b41a8e0bb2e34183e00ab8bae9d02770d1ba
diegargon <kn@envigo.net>
2016-08-05 18:59:09 +0200

    check load before started optional plugins

--- plugins/ExampleWeb/ExampleWeb.php
+++ plugins/ExampleWeb/ExampleWeb.php
@@ -28,10 +28,10 @@ function ex_index_page(){
 
 function ex_news_page() {
     plugin_manual_start("NewsAds");
-    plugin_manual_start("NewsVote");
+    its_server_stressed() ? false : plugin_manual_start("NewsVote");
     plugin_manual_start("NewsMedia");
     plugin_manual_start("Newspage");
-    plugin_manual_start("NewsComments");
+    its_server_stressed() ? false : plugin_manual_start("NewsComments");
     
     news_page();
 }
