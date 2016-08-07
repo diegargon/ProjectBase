dc1afbb609d6b24cf1883c658b63fe439de2e193
diegargon <kn@envigo.net>
2016-08-07 18:20:33 +0200

    minor fix

--- plugins/Newspage/tpl/NewsSocialmeta.tpl.php
+++ plugins/Newspage/tpl/NewsSocialmeta.tpl.php
@@ -1,11 +1,9 @@
 <?php
-
 /* 
  *  Copyright @ 2016 Diego Garcia
  */
-
 ?>
-<meta name="news_keywords" content="<?php print $data['tags'] ?> " />
+<meta name="news_keywords" content="<?php !empty($data['tags']) ? print $data['tags'] : false ?> " />
 <meta property="og:title" content="<?php print $data['title'] ?>"/>
 <meta property="og:url" content="<?php print $data['url'] ?>"/>
 <meta property="og:site_name" content="<?php print $data['web_title'] ?>"/>
