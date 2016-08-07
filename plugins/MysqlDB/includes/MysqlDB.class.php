fd8f635f77ced303d1ddd4e13ddf4e8c4a2b2bb0
diegargon <kn@envigo.net>
2016-08-07 14:23:26 +0200

    minor simplified

--- plugins/MysqlDB/includes/MysqlDB.class.php
+++ plugins/MysqlDB/includes/MysqlDB.class.php
@@ -6,7 +6,7 @@ if (!defined('IN_WEB')) { exit; }
 
 class Database {
     var $dblink;
-    
+
     function connect() {
         $this->dblink = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB);
         if (!$this->dblink) {
@@ -14,32 +14,33 @@ class Database {
             exit();
         }   
         $this->query("SET NAMES ". DB_CHARSET ."");
-    
+
         return true;
     }
 
-    function query($string) {        
+    function query($string) {
         $query = $this->dblink->query($string) or $this->dbdie($query);
         return $query;
     }
 
     function fetch($query) {
-    	return $row = $query->fetch_assoc();		
+    	return $row = $query->fetch_assoc();
     }
 
-    function escape($var) { 
-        return $this->dblink->real_escape_string($var);        
+    function escape($var) {
+        return $this->dblink->real_escape_string($var);
     }
-    function escape_strip($var) { 
+
+    function escape_strip($var) {
         $var = strip_tags($var);
-        return $this->dblink->real_escape_string($var);        
+        return $this->dblink->real_escape_string($var);
     }
-    
+
     function num_rows($query) {
 	return $query->num_rows;
     }
 
-    function close() {    
+    function close() {
 	!$this->dblink ?  die('Could not connect: ' . $this->dblink->error) : false;
 	$this->dblink->close();
     }
@@ -63,54 +64,47 @@ class Database {
     }
 
     function free(& $query) {
-        $query->free();        
+        $query->free();
     }
 
     function get_next_num ($table, $field) {
         global $config;
-       
+
         if (empty($table) || empty($field)) { return false; }
-        
+
         $table = $config['DB_PREFIX'] . $table;
-        $q = "SELECT MAX( $field ) AS max FROM `$table`;";    
+        $q = "SELECT MAX( $field ) AS max FROM `$table`;";
         $query = $this->query($q);
         $row = $this->fetch($query);
-    
+
         return ++$row['max'];
-    }    
-    
+    }
+
     /* $db->select_all("users", array('uid' => 1, 'username' => "myname"), "LIMIT 1"); */
     /* Especify operator default '=';
     /* $query = $db->select_all("news", array ("frontpage" => array("value"=> 1, "operator" => "="), "moderation" => 0, "disabled" => 0));
     /* extra not array */
-    function select_all($table, $where = null, $extra = null, $logic = "AND") { 
+    function select_all($table, $where = null, $extra = null, $logic = "AND") {
         global $config;
 
         if(empty($table)) {
             return false;
-        }        
-        
+        }
+
         $q = "SELECT * FROM {$config['DB_PREFIX']}$table";
-        
+
         if (!empty($where)) {
             $q .= " WHERE ";
-            foreach ($where as $field => $value){
-                if (!is_array($value)) {
-                 $q_where_fields[] = "$field = " . "'". $value ."'";
-                } else {
-                    $q_where_fields[] = "$field {$value['operator']} " . "'". $value['value'] ."'";
-                }
-            }
-            $q .= implode( " $logic ", $q_where_fields );
-        }                
-        !empty($extra) ? $q .= " $extra" : falsE;
-        
+            $q .= $this->where_process($where, $logic);
+        }
+
+        !empty($extra) ? $q .= " $extra" : false;
+
         return $this->query($q);
     }
-    
     /*
      * 
-     */   
+     */
     function search($table, $s_fields, $searchText, $where, $extra = null) {
         global $config;
 
@@ -122,20 +116,13 @@ class Database {
         $q = "SELECT * FROM {$config['DB_PREFIX']}$table WHERE ";
 
         if (!empty($where)) {
-            foreach ($where as $field => $value){
-                if (!is_array($value)) {
-                 $q_where_fields[] = "$field = " . "'". $value ."'";
-                } else {
-                    $q_where_fields[] = "$field {$value['operator']} " . "'". $value['value'] ."'";
-                }
-            }
-            $q .= implode( " AND ", $q_where_fields );
+            $q .= $this->where_process($where, $logic = "AND");
             $q .= " AND ";
         }
-        
+
         foreach ($fields_ary as $field) {
             !empty($where_s_fields) ? $where_s_fields .= " OR " : false;
-        
+
             foreach ($s_words_ary as $s_word) {
                 if (mb_strlen($s_word) > $config['L_SEARCH_MIN_CHAR']) {
                     !empty($where_s_tmp) ? $where_s_tmp .= " AND " : false;
@@ -145,7 +132,7 @@ class Database {
             !empty($where_s_tmp) ? $where_s_fields .= $where_s_tmp : false;
             $where_s_tmp = "";
         }
-        
+
         if (!empty($where_s_fields) ) {
             $q .= "(". $where_s_fields .")";
         } else {
@@ -164,61 +151,60 @@ class Database {
         $q = "UPDATE {$config['DB_PREFIX']}$table SET ";
 
         if(empty($set) || empty($table)) { return false; }
-        
+
         foreach ($set as $field => $value) {
              $q_set_fields[] = "$field = " . "'". $value ."'";
-        } 
+        }
         $q .= implode( ',', $q_set_fields );
-        
+
         if (!empty($where)) {
             $q .= " WHERE ";
-            foreach ($where as $field => $value){
-                if (!is_array($value)) {
-                 $q_where_fields[] = "$field = " . "'". $value ."'";
-                } else {
-                    $q_where_fields[] = "$field {$value['operator']} " . "'". $value['value'] ."'";
-                }
-            }
-            $q .= implode( " $logic ", $q_where_fields );
-        } 
+            $q .= $this->where_process($where, $logic);
+        }
+
         !empty($extra) ? $q .= " $extra" : false;
         return $this->query($q);
     }
-    
     /*  */
     function insert($table, $data) {
         global $config;
 
         if (empty($table) || empty($data)) { return false; }
-        
-        foreach ($data as $field => $value){        
+
+        foreach ($data as $field => $value){
             $fields_ary[] = $field;
             $values_ary[] = "'". $value . "'";
         }
         $fields = implode( ', ', $fields_ary );
         $values = implode( ', ', $values_ary );
-        $q = "INSERT INTO {$config['DB_PREFIX']}$table ( $fields ) VALUES ( $values )";        
-                
+        $q = "INSERT INTO {$config['DB_PREFIX']}$table ( $fields ) VALUES ( $values )";
+
         return $this->query($q);
     }
-    
+
     function delete($table, $where, $extra = null, $logic = 'AND') {
         global $config;
-               
-        if(empty($table) || empty($where) ) { return false; }
-        
-        $q = "DELETE FROM {$config['DB_PREFIX']}$table WHERE ";
 
-        foreach ($where as $field => $value) {
-            if(!is_array($value)) {
+        if(empty($table) || empty($where) ) { return false; }
+
+        $q = "DELETE FROM {$config['DB_PREFIX']}$table WHERE ";
+        $q .= $this->where_process($where, $logic);
+
+        !empty($extra) ? $q .= " $extra" : false;
+
+        return $this->query($q);
+    }
+
+    private function where_process($where, $logic) {
+
+        foreach ($where as $field => $value){
+            if (!is_array($value)) {
                 $q_where_fields[] = "$field = " . "'". $value ."'";
             } else {
-                $q_where_fields[] = "$field {$value['operator']} " . "'". $value['value'] ."'";                
+                $q_where_fields[] = "$field {$value['operator']} " . "'". $value['value'] ."'";
             }
-        } 
-        $q .= implode( " $logic ", $q_where_fields );        
-        !empty($extra) ? $q .= " $extra" : false;
-        
-        return $this->query($q);
+        }
+        $q = implode( " $logic ", $q_where_fields );
+        return $q;
     }
 }
