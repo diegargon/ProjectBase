d574ded86b4c49e8ba85d859033aa19427b189cb
diegargon <kn@envigo.net>
2016-08-20 03:58:04 +0200

    add og:image

--- plugins/Newspage/tpl/NewsSocialmeta.tpl.php
+++ plugins/Newspage/tpl/NewsSocialmeta.tpl.php
@@ -7,5 +7,8 @@
 <meta property="og:title" content="<?php print $data['title'] ?>"/>
 <meta property="og:url" content="<?php print $data['url'] ?>"/>
 <meta property="og:site_name" content="<?php print $data['web_title'] ?>"/>
-<?php /* <meta property="og:image" content="" /> */ ?>
+<meta property="og:type" content="article" />
+<?php if(!empty($data['mainimage'])){  ?>
+<meta property="og:image" content="<?php print $data['mainimage'] ?>" /> 
+<?php }?>
 <meta property="og:description" content="<?php print $data['lead'] ?> "/>
