4e273109a19fec207f9f3df5a3242ebd57706a9c
diegargon <kn@envigo.net>
2016-08-22 12:37:57 +0200

    css fix, responsive

--- plugins/Newspage/tpl/news_body_style2.tpl.php
+++ plugins/Newspage/tpl/news_body_style2.tpl.php
@@ -6,14 +6,14 @@ if (!defined('IN_WEB')) { exit; }
 ?>
         <div  class="clear bodysize page">   
             <?php isset($tpldata['ADD_TOP_NEWS']) ? print $tpldata['ADD_TOP_NEWS']:false ?>                         
-            <section id="col1">
+            <section class="col1">
                <?php   isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false ?>
             </section>
-            <section id="col2">
+            <section class="col2">
                <?php isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false ?> 
                <?php   isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] :false ?>
             </section>
-            <section id="col3">
+            <section class="col3">
                <?php   isset($tpldata['COL3_ARTICLES']) ? print $tpldata['COL3_ARTICLES'] : false ?>
             </section>            
             <?php isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS']:false ?>            
