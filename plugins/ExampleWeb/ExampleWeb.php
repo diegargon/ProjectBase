c5da87b9b231edf9420de54188a62799b2dcca1b
diegargon <kn@envigo.net>
2016-08-25 19:12:51 +0200

    fix newsupload missing feature in edit&newpage, format code and add few hooks for each section in news.php

--- plugins/ExampleWeb/ExampleWeb.php
+++ plugins/ExampleWeb/ExampleWeb.php
@@ -4,35 +4,36 @@
  */
 if (!defined('IN_WEB')) { exit; }
 
-function ExampleWeb_init(){
-        
-    print_debug ("ExampleWeb initialized", "PLUGIN_LOAD");
-            
+function ExampleWeb_init() {
+
+    print_debug("ExampleWeb initialized", "PLUGIN_LOAD");
+
     includePluginFiles("ExampleWeb");
-    
+
     register_uniq_action("index_page", "ex_index_page");
-    register_action("common_web_structure", "ex_common_web_structure", 5);
-    register_action("preload_Newspage_submitnews", "ex_Newspage_submitnews", 5);
-    register_action("preload_Newspage_news", "ex_Newspage_news", 5);
+    register_action("common_web_structure", "ex_common_web_structure");
+    register_action("preload_Newspage_submitnews", "ex_Newspage_submitnews");
+    register_action("begin_newsedit", "ex_Newspage_submitnews");
+    register_action("begin_newspage", "ex_Newspage_submitnews");
+    register_action("preload_Newspage_news", "ex_Newspage_news");
 }
 
 function ex_common_web_structure() {
     plugin_start("DebugWindow");
 }
 
-function ex_index_page(){ 
+function ex_index_page() {
     plugin_start("NewsAds");
     plugin_start("Newspage");
     its_server_stressed() ? false : plugin_start("NewsSearch");
     news_portal();
 }
 
-function ex_Newspage_submitnews () {
+function ex_Newspage_submitnews() {
     plugin_start("NewsMediaUploader");
-    plugin_start("Newspage");
 }
 
-function ex_Newspage_news () {
+function ex_Newspage_news() {
     plugin_start("NewsAds");
     its_server_stressed() ? false : plugin_start("NewsVote");
     its_server_stressed() ? false : plugin_start("NewsSearch");
--- plugins/Newspage/news.php
+++ plugins/Newspage/news.php
@@ -5,12 +5,13 @@
 if (!defined('IN_WEB')) { exit; }
 
 do_action("news_page_begin");
-    
-if(!empty($_GET['newsedit']) && !empty($_GET['lang_id'])) {
+
+if (!empty($_GET['newsedit']) && !empty($_GET['lang_id'])) {
+    do_action("begin_newsedit");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_page_edit.php");
 
-    if( ($news_data = news_check_edit_authorized()) != false) {
+    if (($news_data = news_check_edit_authorized()) != false) {
         if (!empty($_POST['newsFormSubmit_ST2'])) {
             news_form_process($news_data['news_auth']);
         } else if (!empty($_POST['preview'])) {
@@ -28,24 +29,26 @@ if(!empty($_GET['newsedit']) && !empty($_GET['lang_id'])) {
         do_action("common_web_structure"); // error messsage box already set
     }
 } else if (!empty($_GET['news_new_lang'])) {
+    do_action("begin_news_new_lang");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_new_lang.php");
-    if (defined('MULTILANG') && !empty($_POST['post_newlang']) && empty($_POST['preview']) ) {
+    if (defined('MULTILANG') && !empty($_POST['post_newlang']) && empty($_POST['preview'])) {
         news_form_newlang_process();
     } else if (!empty($_POST['preview'])) {
-        news_form_preview ();            
+        news_form_preview();
     } else if (defined('MULTILANG')) {
         do_action("common_web_structure");
         Newspage_FormScript();
         news_new_lang();
     }
 } else if (!empty($_GET['newpage'])) {
+    do_action("begin_newspage");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_new_page.php");
-    if(!empty($_POST['num_pages']) && empty($_POST['preview'])) {
+    if (!empty($_POST['num_pages']) && empty($_POST['preview'])) {
         news_newpage_form_process();
     } else if (!empty($_POST['preview'])) {
-        news_form_preview ();
+        news_form_preview();
     } else {
         do_action("common_web_structure");
         Newspage_FormPageScript();
