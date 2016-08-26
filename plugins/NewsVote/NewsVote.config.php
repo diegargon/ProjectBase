f6ae81e284864a2aa72058a91a4a187b3ff1b121
diegargon <kn@envigo.net>
2016-08-26 14:22:23 +0200

    url TODO work done

--- plugins/NewsVote/NewsVote.config.php
+++ plugins/NewsVote/NewsVote.config.php
@@ -8,4 +8,5 @@ define("NEWS_VOTE", true);
 
 $config['NEWSVOTE_ON_NEWS'] = 1;
 $config['NEWSVOTE_ON_NEWS_COMMENTS'] = 1;
+$config['NEWSVOTE_STARS_URL'] = "/plugins/NewsVote/tpl/images/";
 //$config[''] = ;
