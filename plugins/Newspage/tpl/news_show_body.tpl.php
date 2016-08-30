6a301e363b97a7989c57dc8265b8ad59b3a47455
diegargon <kn@envigo.net>
2016-08-30 11:30:31 +0200

    change isset/!empty

--- plugins/Newspage/tpl/news_show_body.tpl.php
+++ plugins/Newspage/tpl/news_show_body.tpl.php
@@ -7,14 +7,14 @@ if (!defined('IN_WEB')) { exit; }
 <div id="news_container" class="newsrow">
     <div  class="clear bodysize page">
         <?php
-        if (isset($data['news_msg']) && !empty($data['news_msg'])) {
+        if (!empty($data['news_msg']) && !empty($data['news_msg'])) {
             ?>
             <div class="news_msg"><p><?php print $data['news_msg'] ?></p></div>
         <?php } ?>
-        <?php isset($tpldata['ADD_TO_NEWSSHOW_TOP']) ? print $tpldata['ADD_TO_NEWSSHOW_TOP'] : false ?>
+        <?php !empty($tpldata['ADD_TO_NEWSSHOW_TOP']) ? print $tpldata['ADD_TO_NEWSSHOW_TOP'] : false ?>
         <section class="article_body">
             <h1>
-                <?php isset($data['title']) ? print $data['title'] : false ?>
+                <?php !empty($data['title']) ? print $data['title'] : false ?>
             </h1>                
             <?php if (!empty($data['news_admin_nav'])) { ?>
                 <div id='adm_nav_container'>
@@ -29,18 +29,18 @@ if (!defined('IN_WEB')) { exit; }
                 <?php if (!empty($data['author_avatar'])) { ?> 
                     <div class='avatar'><img width='50' src='<?php print $data['author_avatar']; ?>' alt='' /></div>                        
                 <?php } ?>
-                <?php isset($tpldata['ADD_NEWS_INFO_POST_AVATAR']) ? print $tpldata['ADD_NEWS_INFO_POST_AVATAR'] : false ?>
+                <?php !empty($tpldata['ADD_NEWS_INFO_POST_AVATAR']) ? print $tpldata['ADD_NEWS_INFO_POST_AVATAR'] : false ?>
                 <div class="extra-small">
                     <?php print $data['date'] ?> <br/>
                     <a href='/<?php print $config['WEB_LANG'] ?>/profile&viewprofile=<?php print $data['author_uid'] ?>'><?php print $data['author'] ?></a>
                     <?php
-                    isset($data['translator']) ? print " | " . $LANGDATA['L_NEWS_TRANSLATE_BY'] . $data['translator'] : false;
+                    !empty($data['translator']) ? print " | " . $LANGDATA['L_NEWS_TRANSLATE_BY'] . $data['translator'] : false;
                     ?>
-                    <?php if (isset($data['news_sources'])) { ?>
+                    <?php if (!empty($data['news_sources'])) { ?>
                         | <span><?php print $LANGDATA['L_NEWS_SOURCE'] . ": " . $data['news_sources'] ?> </span>
                     <?php } ?>
                 </div>
-                <?php isset($tpldata['ADD_NEWS_INFO_BOTTOM']) ? print $tpldata['ADD_NEWS_INFO_BOTTOM'] : false ?>
+                <?php !empty($tpldata['ADD_NEWS_INFO_BOTTOM']) ? print $tpldata['ADD_NEWS_INFO_BOTTOM'] : false ?>
             </div>
             <?php if (!empty($data['lead'])) { ?>
                 <p class="article_lead">
@@ -50,7 +50,7 @@ if (!defined('IN_WEB')) { exit; }
             <hr/>
             <?php !empty($tpldata['NEWS_MAIN_PRE_TEXT']) ? print $tpldata['NEWS_MAIN_PRE_TEXT'] : false; ?>
             <div class="article_text">
-                <?php isset($data['text']) ? print $data['text'] : false ?>
+                <?php !empty($data['text']) ? print $data['text'] : false ?>
             </div>
             <?php !empty($tpldata['NEWS_MAIN_AFTER_TEXT']) ? print $tpldata['news_main__pre_text'] : false; ?>
             <?php if (!empty($data['news_related'])) {
@@ -62,7 +62,7 @@ if (!defined('IN_WEB')) { exit; }
                 </div>
             <?php } ?>
             <?php !empty($data['pager']) ? print $data['pager'] : false ?>
-            <?php isset($tpldata['ADD_TO_NEWSSHOW_BOTTOM']) ? print $tpldata['ADD_TO_NEWSSHOW_BOTTOM'] : false ?>                        
+            <?php !empty($tpldata['ADD_TO_NEWSSHOW_BOTTOM']) ? print $tpldata['ADD_TO_NEWSSHOW_BOTTOM'] : false ?>                        
         </section>
         <div class="article_side">
             <?php !empty($tpldata['ADD_TO_NEWS_SIDE']) ? print $tpldata['ADD_TO_NEWS_SIDE'] : false ?>
