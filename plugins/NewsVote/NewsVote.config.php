c9821c2211da6c0b73126c6285d13067a4f3313a
diegargon <kn@envigo.net>
2016-08-30 13:28:01 +0200

    add give points to user for each news or comment rate

--- plugins/NewsVote/NewsVote.config.php
+++ plugins/NewsVote/NewsVote.config.php
@@ -9,4 +9,8 @@ define("NEWS_VOTE", true);
 $config['NEWSVOTE_ON_NEWS'] = 1;
 $config['NEWSVOTE_ON_NEWS_COMMENTS'] = 1;
 $config['NEWSVOTE_STARS_URL'] = "/plugins/NewsVote/tpl/images/";
+$config['NEWSVOTE_NEWS_USER_RATING_N'] = 1; //News author
+$config['NEWSVOTE_NEWS_USER_RATING_NT'] = 1; //News translator;
+$config['NEWSVOTE_COMMENT_USER_RATING'] = 1;
+$config['NEWSVOTE_COMMENT_USER_RATING_MODE'] = "div2"; //1 mean one point, div2 divide/2 (round and 1 point at least), 0 = real user rating
 //$config[''] = ;
--- plugins/NewsVote/NewsVote.php
+++ plugins/NewsVote/NewsVote.php
@@ -44,7 +44,7 @@ function newsvote_page_begin() {
 }
 
 function newsvote_rate_news($user, $user_rate) {
-    global $db, $LANGDATA;
+    global $db, $config, $LANGDATA;
 
     $news['nid'] = S_POST_INT("rate_rid", 11, 1);
     $news['lang_id'] = S_POST_INT("rate_lid", 11, 1);
@@ -65,12 +65,15 @@ function newsvote_rate_news($user, $user_rate) {
         );
         $db->insert("rating_track", $insert_ary);
         NewsVote_Calc_Rating($news['nid'], $news['lang_id'], "news_rate"); //TODO: LIMIT USE THIS
+        if ($config['NEWSVOTE_NEWS_USER_RATING_N']) {
+            newsvote_news_user_rating($news['nid'], $news['lang_id'], $user_rate);
+        }
     }
     die('[{"status": "3", "msg": "' . $LANGDATA['L_VOTE_SUCCESS'] . '"}]');
 }
 
 function newsvote_rate_comment($user, $user_rate) {
-    global $db, $LANGDATA;
+    global $db, $config, $LANGDATA;
 
     $comment['cid'] = S_POST_INT("rate_rid", 11, 1);
     $comment['lang_id'] = S_POST_INT("rate_lid", 11, 1);
@@ -91,6 +94,9 @@ function newsvote_rate_comment($user, $user_rate) {
         );
         $db->insert("rating_track", $insert_ary);
         NewsVote_Calc_Rating($comment['cid'], $comment['lang_id'], "news_comments_rate"); //TODO: LIMIT THIS
+        if ($config['NEWSVOTE_COMMENT_USER_RATING']) {
+            newsvote_comment_user_rating($comment['cid'], $comment['lang_id'], $user_rate);
+        }
     }
     die('[{"status": "6", "msg": "' . $LANGDATA['L_VOTE_SUCCESS'] . '"}]');
 }
@@ -148,4 +154,42 @@ function newsvote_news_addrate($news) {
     $rate_data = array_merge($rate_data, $news, $rate_stars);
     $rate_content = $tpl->getTpl_file("NewsVote", "news_rate", $rate_data);
     $tpl->addto_tplvar("ADD_NEWS_INFO_POST_AVATAR", $rate_content);
+}
+
+function newsvote_news_user_rating($nid, $lang_id, $user_rating) {
+    global $db, $config;
+
+    $query = $db->select_all("news", array("nid" => "$nid", "lang_id" => $lang_id, "page" => 1), "LIMIT 1");
+    $news_data = $db->fetch($query);
+    $author_xtrData = uXtra_get($news_data['author_id']);
+    $new_rating = $author_xtrData['rating_user'] + $user_rating;
+    $new_rating_times = ++$author_xtrData['rating_times'];
+
+    uXtra_upsert(array("rating_user" => "$new_rating", "rating_times" => "$new_rating_times"), array("uid" => $author_xtrData['uid']));
+
+    if (!empty($news_data['translator_id']) && $config['NEWSVOTE_NEWS_USER_RATING_NT'] && $news_data['moderation'] == 0) {
+        $translator_xtrData = uXtra_get($news_data['translator_id']);
+        $t_new_rating = $translator_xtrData['rating_user'] + $user_rating;
+        $t_new_rating_times = ++$translator_xtrData['rating_times'];
+        uXtra_upsert(array("rating_user" => "$t_new_rating", "rating_times" => "$t_new_rating_times"), array("uid" => $translator_xtrData['uid']));
+    }
+}
+
+function newsvote_comment_user_rating($cid, $lang_id, $user_rating) {
+    global $db, $config;
+
+    $query = $db->select_all("comments", array("cid" => "$cid", "lang_id" => $lang_id), "LIMIT 1");
+    $comment_data = $db->fetch($query);
+    $author_xtrData = uXtra_get($comment_data['author_id']);
+    if ($config['NEWSVOTE_COMMENT_USER_RATING_MODE'] == 1) {
+        $new_rating = ++$author_xtrData['rating_user'];
+    } else if ($config['NEWSVOTE_COMMENT_USER_RATING_MODE'] == "div2") {
+        $new_rating = $author_xtrData['rating_user'] + round($user_rating / 2);
+        $new_rating == 0 ? $new_rating = 1 : false;
+    } else {
+        $new_rating = $author_xtrData['rating_user'] + $user_rating;
+    }
+    $new_rating_times = ++$author_xtrData['rating_times'];
+
+    uXtra_upsert(array("rating_user" => "$new_rating", "rating_times" => "$new_rating_times"), array("uid" => $author_xtrData['uid']));
 }
