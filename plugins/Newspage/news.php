c69bed09981fddfb2ba643e155b86c08eb089d0f
diegargon <kn@envigo.net>
2016-08-31 14:30:38 +0200

    simplify

--- plugins/Newspage/news.php
+++ plugins/Newspage/news.php
@@ -1,56 +1,49 @@
 <?php
+
 /*
  *  Copyright @ 2016 Diego Garcia	
-*/
-if (!defined('IN_WEB')) { exit; }
+ */
+!defined('IN_WEB') ? exit : true;
 
 do_action("news_page_begin");
 
-if (!empty($_GET['newsedit']) && !empty($_GET['lang_id'])) {
+!empty($_POST['preview']) ? news_form_preview() : null;
+
+if (!empty($_GET['newsedit']) && !empty($_GET['lang_id']) && empty($_POST['preview'])) {
     do_action("begin_newsedit");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_page_edit.php");
 
-    if (!empty($_POST['news_update']) && empty($_POST['preview'])) {
+    if (!empty($_POST['news_update'])) {
         news_form_edit_process();
-    } else if (!empty($_POST['preview'])) {
-        news_form_preview();
     } else {
         do_action("common_web_structure");
-        if (!empty($_GET['npage']) && $_GET['npage'] > 1) {
-            Newspage_FormPageScript();
-        } else {
-            Newspage_FormScript();
-        }
+        !empty($_GET['npage']) && $_GET['npage'] > 1 ? Newspage_FormPageScript() : Newspage_FormScript();
         news_edit();
     }
-} else if (!empty($_GET['news_new_lang'])) {
+} else if (defined('MULTILANG') && !empty($_GET['news_new_lang']) && empty($_POST['preview'])) {
     do_action("begin_news_new_lang");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_new_lang.php");
-    if (defined('MULTILANG') && !empty($_POST['post_newlang']) && empty($_POST['preview'])) {
+    if (!empty($_POST['post_newlang'])) {
         news_form_newlang_process();
-    } else if (!empty($_POST['preview'])) {
-        news_form_preview();
-    } else if (defined('MULTILANG')) {
+    } else {
         do_action("common_web_structure");
         Newspage_FormScript();
         news_new_lang();
     }
-} else if (!empty($_GET['newpage'])) {
+} else if (!empty($_GET['newpage']) && empty($_POST['preview'])) {
     do_action("begin_newspage");
     require_once ("includes/news_form.common.php");
     require_once ("includes/news_new_page.php");
-    if (!empty($_POST['new_page']) && empty($_POST['preview'])) {
+    if (!empty($_POST['new_page'])) {
         news_newpage_form_process();
-    } else if (!empty($_POST['preview'])) {
-        news_form_preview();
     } else {
         do_action("common_web_structure");
         Newspage_FormPageScript();
         news_new_page();
     }
-} else {
+} else if (empty($_POST['preview'])) {
     do_action("begin_newsshow");
     require_once("includes/news_page.main.php");
     require_once("includes/parser.class.php");
