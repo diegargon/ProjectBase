dbae62c720ee3412bdf6c94b4e8afcdc7fbc0957
diegargon <kn@envigo.net>
2016-09-01 16:44:46 +0200

    minor change and format code

--- plugins/Newspage/tpl/news_body_style3.tpl.php
+++ plugins/Newspage/tpl/news_body_style3.tpl.php
@@ -1,25 +1,27 @@
 <?php
-/* 
+/*
  *  Copyright @ 2016 Diego Garcia
  */
 !defined('IN_WEB') ? exit : true;
 ?>
-        <div  class="clear bodysize page">
-            <?php isset($tpldata['ADD_TOP_NEWS']) ? print $tpldata['ADD_TOP_NEWS']:false ?>
-            <?php isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false ?>
-<?php
-if ($config['NEWS_PORTAL_COLS'] >= 1) {
-?>
-            <section class="col col2">
-               <?php   isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false ?>
-            </section>
-<?php
-}
-if ($config['NEWS_PORTAL_COLS'] >= 2) {
-?>
-            <section class="col col2">            
-               <?php   isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] :false ?>
-            </section>
-<?php } ?>
-            <?php isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS']:false ?>
-        </div>
+<div  class="clear bodysize page">
+    <?php
+    isset($tpldata['ADD_TOP_NEWS']) ? print $tpldata['ADD_TOP_NEWS'] : false;
+    isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false;
+    if ($config['NEWS_PORTAL_COLS'] >= 1) {
+        ?>
+        <section class="col col2">
+            <?php isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false ?>
+        </section>
+        <?php
+    }
+    if ($config['NEWS_PORTAL_COLS'] >= 2) {
+        ?>
+        <section class="col col2">            
+            <?php isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] : false ?>
+        </section>
+        <?php
+    }
+    isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS'] : false;
+    ?>
+</div>
