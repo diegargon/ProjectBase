ea6be7346da0d3f1d77e79b2ca7e44a3ef0dc1cf
diegargon <kn@envigo.net>
2016-09-01 16:43:13 +0200

    minor change and format code

--- plugins/Newspage/tpl/news_body_style2.tpl.php
+++ plugins/Newspage/tpl/news_body_style2.tpl.php
@@ -1,32 +1,36 @@
 <?php
-/* 
+/*
  *  Copyright @ 2016 Diego Garcia
  */
 !defined('IN_WEB') ? exit : true;
 ?>
-        <div  class="clear bodysize page">
-            <?php isset($tpldata['ADD_TOP_NEWS']) ? print $tpldata['ADD_TOP_NEWS']:false ?>
-<?php
-if ($config['NEWS_PORTAL_COLS'] >= 1) {
-?>
-            <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
-               <?php   isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false ?>
-            </section>
-<?php
-}
-if ($config['NEWS_PORTAL_COLS'] >= 2) {
-?>
-            <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
-            <?php isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false ?>
-               <?php   isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] :false ?>
-            </section>
-<?php
-}
-if ($config['NEWS_PORTAL_COLS'] >= 3) {
-?>
-            <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
-               <?php   isset($tpldata['COL3_ARTICLES']) ? print $tpldata['COL3_ARTICLES'] : false ?>
-            </section>
-<?php } ?>
-            <?php isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS']:false ?>
-        </div>
+<div  class="clear bodysize page">
+    <?php
+    isset($tpldata['ADD_TOP_NEWS']) ? print $tpldata['ADD_TOP_NEWS'] : false;
+    if ($config['NEWS_PORTAL_COLS'] >= 1) {
+        ?>
+        <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
+            <?php
+            isset($tpldata['FEATURED']) ? print $tpldata['FEATURED'] : false;
+            isset($tpldata['COL1_ARTICLES']) ? print $tpldata['COL1_ARTICLES'] : false;
+            ?>
+        </section>
+        <?php
+    }
+    if ($config['NEWS_PORTAL_COLS'] >= 2) {
+        ?>
+        <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
+            <?php isset($tpldata['COL2_ARTICLES']) ? print $tpldata['COL2_ARTICLES'] : false ?>
+        </section>
+        <?php
+    }
+    if ($config['NEWS_PORTAL_COLS'] >= 3) {
+        ?>
+        <section class="col col<?php print $config['NEWS_PORTAL_COLS'] ?>">
+            <?php isset($tpldata['COL3_ARTICLES']) ? print $tpldata['COL3_ARTICLES'] : false ?>
+        </section>
+        <?php
+    }
+    isset($tpldata['ADD_BOTTOM_NEWS']) ? print $tpldata['ADD_BOTTOM_NEWS'] : false
+    ?>
+</div>
