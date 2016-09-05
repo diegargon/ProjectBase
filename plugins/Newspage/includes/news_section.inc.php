07950aa52d1a0268aaec8b04833ced1723d0fc1a
diegargon <kn@envigo.net>
2016-09-05 20:11:10 +0200

    check parent category

--- plugins/Newspage/includes/news_section.inc.php
+++ plugins/Newspage/includes/news_section.inc.php
@@ -8,6 +8,8 @@
 function getCatIDbyName($catname) {
     global $db;
 
+    $catname = $db->escape_strip($catname);
+
     $query = $db->select_all("categories", array("name" => $catname), "LIMIT 1");
     if ($db->num_rows($query) <= 0) {
         return false;
@@ -16,21 +18,42 @@ function getCatIDbyName($catname) {
     return $cat_data['cid'];
 }
 
-function getCatIDbyName_LIST($cat_list) {
+function getCatbyName($catname, $catparent = null) {
     global $db;
-
-    //FIX: That give problems/conflict with same category name in other sections
-    //check and get father match one    
-    $cat_list = explode(".", $cat_list);
-    $catname = end($cat_list);
 
     $catname = $db->escape_strip($catname);
 
-    $query = $db->select_all("categories", array("name" => $catname), "LIMIT 1");
+    $where_ary = array(
+        "name" => $catname,
+    );
+    if (!empty($catparent)) {
+        $catparent = $db->escape_strip($catparent);
+        $catparent_id = getCatIDbyName($catparent);
+        !empty($catparent_id) ? $where_ary['father'] = $catparent_id : null;
+    }
+
+    $query = $db->select_all("categories", $where_ary, "LIMIT 1");
     if ($db->num_rows($query) <= 0) {
         return false;
     }
-    $cat_data = $db->fetch($query);
+    return $db->fetch($query);
+}
+
+function getCatIDbyName_LIST($cat_list) {
+    global $db;
+
+    
+    //check parent to avoid errors on duplicate name != section    
+    $cat_list = explode(".", $cat_list);
+    if (count($cat_list) > 1) {
+        $catname = array_pop($cat_list);
+        $catparent = array_pop($cat_list);
+        $cat_data = getCatbyName($catname, $catparent);
+    } else {
+        $catname = array_pop($cat_list);
+        $cat_data = getCatbyName($catname);
+    }
+
     return $cat_data['cid'];
 }
 
