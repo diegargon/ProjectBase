25ca7e93593fedc76f3a5f1d526b4e1d742142cc
diegargon <diego@envigo.net>
2016-12-07 00:49:59 +0100

    icons

--- plugins/tplBasic/tpl/head.tpl.php
+++ plugins/tplBasic/tpl/head.tpl.php
@@ -19,7 +19,15 @@
         <meta name="author" content="<?php print $config['PAGE_AUTHOR'] ?> " />
         <meta name="distribution" content="global"  />
         <meta name="resource-type" content="document"  />
-        <meta name="organization" content="<?php print $config['WEB_NAME'] ?> "/>        
+        <meta name="organization" content="<?php print $config['WEB_NAME'] ?> "/> 
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#ffffff">
+
+        
         <?php
         isset($tpldata['HEAD']) ? print $tpldata['HEAD'] : false;
         isset($tpldata['META']) ? print $tpldata['META'] : false;
