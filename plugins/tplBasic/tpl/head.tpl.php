694aa47f5319ebb12a88dccad9ba2712e6f683b6
diegargon <diego@envigo.net>
2016-12-07 00:59:38 +0100

    icons

--- plugins/tplBasic/tpl/head.tpl.php
+++ plugins/tplBasic/tpl/head.tpl.php
@@ -20,18 +20,17 @@
         <meta name="distribution" content="global"  />
         <meta name="resource-type" content="document"  />
         <meta name="organization" content="<?php print $config['WEB_NAME'] ?> "/> 
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-        <link rel="manifest" href="/manifest.json">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <meta name="theme-color" content="#ffffff">
+        <meta name="theme-color" content="#ffffff" />
 
-        
         <?php
         isset($tpldata['HEAD']) ? print $tpldata['HEAD'] : false;
         isset($tpldata['META']) ? print $tpldata['META'] : false;
         ?>
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php print $config['WEB_URL'] ?>apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="<?php print $config['WEB_URL'] ?>favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="<?php print $config['WEB_URL'] ?>favicon-16x16.png" sizes="16x16" />
+        <link rel="manifest" href="<?php print $config['WEB_URL'] ?>manifest.json" />
+        <link rel="mask-icon" href="<?php print $config['WEB_URL'] ?>safari-pinned-tab.svg" />        
         <link rel="shortcut icon" href="<?php print $config['WEB_URL'] ?>favicon.ico" type='image/x-icon' />
         <?php
         isset($tpldata['LINK']) ? print $tpldata['LINK'] : false;
