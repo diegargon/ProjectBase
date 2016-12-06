fca458aa19e3872d83cd84830285bbb301dda8db
diegargon <diego@envigo.net>
2016-12-06 22:47:27 +0100

    add meta

--- plugins/tplBasic/tpl/head.tpl.php
+++ plugins/tplBasic/tpl/head.tpl.php
@@ -8,14 +8,18 @@
 <html dir="<?php print $config['WEB_DIR'] ?>" lang="<?php print $config['WEB_LANG'] ?>">
     <head>
         <title><?php echo $config['PAGE_TITLE'] ?></title>
-        <meta charset="<?php echo $config['CHARSET'] ?>">
-        <meta name='language' content='<?php print $config['WEB_LANG'] ?>'>
-        <meta name="viewport" content="<?php print $config['PAGE_VIEWPORT'] ?>">
-        <meta name='robots' content='index,follow'>        
-        <meta name="keywords" content="<?php print $config['PAGE_KEYWORDS'] ?>">
-        <meta name="referrer" content="always">
-        <meta name="description" content="<?php echo $config['PAGE_DESC'] ?>">
-        <meta name="author" content="<?php print $config['PAGE_AUTHOR'] ?> ">
+        <meta charset="<?php echo $config['CHARSET'] ?>" />
+        <meta name='language' content='<?php print $config['WEB_LANG'] ?>' />
+        <meta name="viewport" content="<?php print $config['PAGE_VIEWPORT'] ?>" />
+        <meta name='robots' content='all' />
+        <meta name="keywords" content="<?php print $config['PAGE_KEYWORDS'] ?>" />
+        <meta name="news_keywords" content="<?php print $config['PAGE_KEYWORDS'] ?>" />
+        <meta name="referrer" content="always" />
+        <meta name="description" content="<?php echo $config['PAGE_DESC'] ?>" />
+        <meta name="author" content="<?php print $config['PAGE_AUTHOR'] ?> " />
+        <meta name="distribution" content="global"  />
+        <meta name="resource-type" content="document"  />
+        <meta name="organization" content="<?php print $config['WEB_NAME'] ?> "/>        
         <?php
         isset($tpldata['HEAD']) ? print $tpldata['HEAD'] : false;
         isset($tpldata['META']) ? print $tpldata['META'] : false;
