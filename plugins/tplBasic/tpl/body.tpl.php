db6350c15961e50d4de2c14ae81d8c47dafafbf3
diegargon <diego@envigo.net>
2016-12-07 03:18:44 +0100

    use WEB_NAME INSTEAD OF TITLE FOR BRAND

--- plugins/tplBasic/tpl/body.tpl.php
+++ plugins/tplBasic/tpl/body.tpl.php
@@ -2,8 +2,8 @@
 !defined('IN_WEB') ? exit : true;
 ?>
 <body id="Top">
-    <div class="container">    
-        <div class="row1">        
+    <div class="container">
+        <div class="row1">
             <header id="header" class="clear">
                 <?php isset($tpldata['ADD_HEADER_BEGIN']) ? print $tpldata['ADD_HEADER_BEGIN'] : false ?>  
                 <?php if ($config['NAV_MENU']) { ?>
@@ -11,25 +11,24 @@
                         function toggleMenu() {
                             document.getElementsByClassName("main-nav")[0].classList.toggle("responsive");
                         }
-                    </script>                        
+                    </script>
                     <nav id="main-nav">
-                        <ul class="main-nav">              
-                            <li class="nav_right resp-icon"><a rel="nofollow" href="javascript:void(0);" onclick="toggleMenu()">&#9776;</a></li>                
+                        <ul class="main-nav">
+                            <li class="nav_right resp-icon"><a rel="nofollow" href="javascript:void(0);" onclick="toggleMenu()">&#9776;</a></li>
                             <?php if ($config['NAV_MENU_HOME']) { ?>
                                 <li class='nav_left lihome'>
                                     <a href='/<?php
                                     $config['FRIENDLY_URL'] ? print $config['WEB_LANG'] : print "?lang={$config['WEB_LANG']}";
-                                    ?>'><img src='<?php print $config['IMG_HOME'] ?>' alt='<?php print $LANGDATA['L_HOME'] ?>' />                        
-                                    </a></li>           
+                                    ?>'><img src='<?php print $config['IMG_HOME'] ?>' alt='<?php print $LANGDATA['L_HOME'] ?>' />
+                                    </a></li>
                             <?php } ?>
                             <?php isset($tpldata['NAV_ELEMENT']) ? print $tpldata['NAV_ELEMENT'] : false; ?>
-
                         </ul>
-                    </nav>            
-                <?php } ?>                
+                    </nav>
+                <?php } ?>
                 <div id="brand">
                     <a href="/<?php $config['FRIENDLY_URL'] ? print $config['WEB_LANG'] : print "?lang={$config['WEB_LANG']}"; ?>">
-                        <?php echo $config['TITLE'] ?></a><br/>
+                        <?php echo $config['WEB_NAME'] ?></a><br/>
                     <span><?php echo $config['WEB_DESC'] ?></span>
                 </div>
                 <?php
@@ -47,7 +46,7 @@
                 if (!empty($tpldata['SECTIONS_SUBMENU'])) {
                     ?>
                     <div id="sections_submenu">
-                        <ul>            
+                        <ul>
                             <?php !empty($tpldata['SECTIONS_SUBMENU']) ? print $tpldata['SECTIONS_SUBMENU'] : null; ?>
                         </ul>
                     </div>
@@ -61,4 +60,3 @@
         isset($tpldata['PRE_ACTION_ADD_TO_BODY']) ? print $tpldata['PRE_ACTION_ADD_TO_BODY'] : false;
         !isset($tpldata['ADD_TO_BODY']) ? print "<p>Hello World</p>" : print $tpldata['ADD_TO_BODY'];
         isset($tpldata['POST_ACTION_ADD_TO_BODY']) ? print $tpldata['POST_ACTION_ADD_TO_BODY'] : false;
-        
