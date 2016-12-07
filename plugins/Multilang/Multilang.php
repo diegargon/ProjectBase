2d0e342823505d7519eccbbd8ba75310a246f50d
diegargon <diego@envigo.net>
2016-12-07 12:37:11 +0100

    format

--- plugins/Multilang/Multilang.php
+++ plugins/Multilang/Multilang.php
@@ -31,7 +31,7 @@ function Multilang_init(){
         if ($config['FRIENDLY_URL']) {
             $request_uri = $config['WEB_URL'] . $config['WEB_LANG'];
         } else {
-        $request_uri = $config['WEB_URL'] . "?lang=" . $config['WEB_LANG'];
+            $request_uri = $config['WEB_URL'] . "?lang=" . $config['WEB_LANG'];
         }
         header('Location:' .$request_uri);
     }
