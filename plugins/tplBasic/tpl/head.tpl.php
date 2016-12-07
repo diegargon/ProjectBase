51785e950521560e4e8b22dd3ffb2edb24250f9d
diegargon <diego@envigo.net>
2016-12-07 12:25:38 +0100

    deprecated shortcut

--- plugins/tplBasic/tpl/head.tpl.php
+++ plugins/tplBasic/tpl/head.tpl.php
@@ -7,14 +7,14 @@
 <!DOCTYPE html>
 <html dir="<?php print $config['WEB_DIR'] ?>" lang="<?php print $config['WEB_LANG'] ?>">
     <head>
-        <title><?php echo $config['PAGE_TITLE'] ?></title>
         <meta charset="<?php echo $config['CHARSET'] ?>" />
-        <meta name='language' content='<?php print $config['WEB_LANG'] ?>' />
         <meta name="viewport" content="<?php print $config['PAGE_VIEWPORT'] ?>" />
+        <title><?php echo $config['PAGE_TITLE'] ?></title>
+        <meta name='language' content='<?php print $config['WEB_LANG'] ?>' />
         <meta name='robots' content='all' />
         <meta name="keywords" content="<?php print $config['PAGE_KEYWORDS'] ?>" />
         <meta name="news_keywords" content="<?php print $config['PAGE_KEYWORDS'] ?>" />
-        <meta name="referrer" content="always" />
+        <meta name="referrer" content="origin-when-crossorigin" />
         <meta name="description" content="<?php echo $config['PAGE_DESC'] ?>" />
         <meta name="author" content="<?php print $config['PAGE_AUTHOR'] ?> " />
         <meta name="distribution" content="global"  />
@@ -31,7 +31,7 @@
         <link rel="icon" type="image/png" href="<?php print $config['WEB_URL'] ?>favicon-16x16.png" sizes="16x16" />
         <link rel="manifest" href="<?php print $config['WEB_URL'] ?>manifest.json" />
         <link rel="mask-icon" href="<?php print $config['WEB_URL'] ?>safari-pinned-tab.svg" />        
-        <link rel="shortcut icon" href="<?php print $config['WEB_URL'] ?>favicon.ico" type='image/x-icon' />
+        <link rel="icon" href="<?php print $config['WEB_URL'] ?>favicon.ico" type='image/x-icon' />
         <?php
         isset($tpldata['LINK']) ? print $tpldata['LINK'] : false;
         isset($tpldata['SCRIPTS_TOP']) ? print $tpldata['SCRIPTS_TOP'] : false;
