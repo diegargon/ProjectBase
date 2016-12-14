fd84db568919c58db245bb4ce95ef5d7865555df
diegargon <diego@envigo.net>
2016-12-14 01:58:18 +0100

    fix title

--- plugins/NewsSearch/tpl/NewsSearch-results.tpl.php
+++ plugins/NewsSearch/tpl/NewsSearch-results.tpl.php
@@ -1,30 +1,31 @@
 <?php
-/* 
+/*
  *  Copyright @ 2016 Diego Garcia
  */
 !defined('IN_WEB') ? exit : true;
 
-if( !empty($data['TPL_FIRST'])) {?>
-<div  class="clear bodysize page">
-    <div id="searchResult">
-        <section>
-            <h2><?php print $LANGDATA['L_NS_SEARCH_RESULT']?></h2>
-            <table class="searchTable">
+if (!empty($data['TPL_FIRST'])) {
+    ?>
+    <div  class="clear bodysize page">
+        <div id="searchResult">
+            <section>
+                <table class="searchTable">
+                    <tr>
+                        <td ><h2><?php print $LANGDATA['L_NS_SEARCH_RESULT'] ?></h2></td>
+                    </tr>
+<?php } ?>
                 <tr>
-                <th class="searchHeader"><?php print $LANGDATA['L_NEWS_TITLE'] ?></th>
-                </tr>
-<?php }?>                
-                <tr>
-                    <td class="">
+                    <td>
                         <a href="<?php print $data['url'] ?>">
-                        <div class="s_news_title"><?php print $data['title'] ?></div>
-                        <div class="s_news_lead"><?php print $data['lead'] ?></div>
+                            <div class="s_news_title"><?php print $data['title'] ?></div>
+                            <div class="s_news_lead"><?php print $data['lead'] ?></div>
                         </a>
                     </td>
                 </tr>
-<?php if(!empty($data['TPL_LAST'])) {?>
-            </table>
-        </section>
+<?php if (!empty($data['TPL_LAST'])) { ?>
+                </table>
+            </section>
+        </div>
     </div>
-</div>
-<?php }
+    <?php
+}
