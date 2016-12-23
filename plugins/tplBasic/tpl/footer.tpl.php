5433296352946356585537ed942b10cd0af895ef
diegargon <diego@envigo.net>
2016-12-23 02:03:56 +0100

    fix url

--- plugins/tplBasic/tpl/footer.tpl.php
+++ plugins/tplBasic/tpl/footer.tpl.php
@@ -5,7 +5,7 @@
     <footer id="footer" class="clear">
         <?php isset($tpldata['ADD_TO_FOOTER']) ? print $tpldata['ADD_TO_FOOTER'] : false; ?>
         <small class="fl_left"><?php print $config['FOOT_COPYRIGHT'] ?></small>
-        <small class="fl_right"><a href=""><?php print $config['TITLE'] ?></a></small>
+        <small class="fl_right"><a href="<?php print $config['WEB_URL'] ?>"><?php print $config['TITLE'] ?></a></small>
     </footer>
 </div>
 </div> <!-- Container -->
