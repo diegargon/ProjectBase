499c1c3a68496eabffee933d96acb4aa3e9ef84a
diegargon <diego@envigo.net>
2016-12-26 15:54:20 +0100

    add option to send raw text not only lang var

--- plugins/tplBasic/tplBasic.php
+++ plugins/tplBasic/tplBasic.php
@@ -45,6 +45,7 @@ function tplBasic_message_box($box_data) {
     !empty($box_data['backlink']) ? $data['BOX_BACKLINK'] = $box_data['backlink'] : $data['BOX_BACKLINK'] = $config['BACKLINK'];
     !empty($box_data['backlink_title']) ? $data['BOX_BACKLINK_TITLE'] = $LANGDATA[$box_data['backlink_title']] : $data['BOX_BACKLINK_TITLE'] = $LANGDATA['L_BACK'];
     $data['BOX_MSG'] = $LANGDATA[$box_data['MSG']];
+    !empty($box_data['XTRA_BOX_MSG']) ? $data['BOX_MSG'] .= $box_data['XTRA_BOX_MSG']: false;
     $tpl->addto_tplvar("ADD_TO_BODY", $tpl->getTPL_file("tplBasic", "msgbox", $data));
 }
 
