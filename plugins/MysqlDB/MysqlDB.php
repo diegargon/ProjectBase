45b14681e9c637b942cd32df483b9666e1f73c9c
diegargon <diego@envigo.net>
2017-07-10 10:13:50 +0200

    remove unused and change to null

--- plugins/MysqlDB/MysqlDB.php
+++ plugins/MysqlDB/MysqlDB.php
@@ -5,15 +5,13 @@
  */
 !defined('IN_WEB') ? exit : true;
 
-global $dblink;
-
 function MysqlDB_Init() {
     global $db, $cfg;
 
     print_debug("MysqlDB Initialice", "PLUGIN_LOAD");
     includePluginFiles("MysqlDB");
 
-    !isset($db) ? $db = new Database : false;
+    !isset($db) ? $db = new Database : null;
     $db->connect($cfg);
     //register_action("finalize", array($db, "close"), "5"); we use destructor now;
 }
