bc7e8cd27df9a3968af6d2bd6a36c744ad287c59
diegargon <diego@envigo.net>
2017-07-11 11:53:32 +0200

    timeutils class

--- includes/time-utils.inc.php
+++ includes/time-utils.inc.php
@@ -1,21 +1,21 @@
 <?php
 
-/* 
+/*
  *  Copyright @ 2016 Diego Garcia
  */
 
-/** TODO CLASS **/
-
+/** TODO CLASS * */
 !defined('IN_WEB') ? exit : true;
 
 class TimeUtils {
+
     private $cfg;
     private $db;
     private $timezone;
     private $server_timezone;
     private $ftime_now;
     private $dateformat;
-    
+
     public function __construct($cfg, $db) {
         $this->cfg = $cfg;
         $this->db = $db;
@@ -24,27 +24,28 @@ class TimeUtils {
         $this->setDateformat();
         $this->ftime_now = new DateTime(date($this->dateformat, time()));
     }
-    
+
     public function format_date($date, $timestamp = false) {
         //TODO DateTime
         if ($timestamp) {
             return date($this->dateformat, $date);
         } else {
             return date($this->dateformat, strtotime($date));
-        }        
+        }
     }
+
     private function setTimezone() {
         //TODO: LANG to TIME ZONE OR DETECT USER TIMEZONE WITH Javascript        
         // OR USER PREF
         date_default_timezone_set($this->cfg['DEFAULT_TIMEZONE']);
         $this->timezone = date_default_timezone_get();
     }
+
     private function setDateFormat() {
         //TODO: Check user preferences
         $this->dateformat = $this->cfg['DEFAULT_DATEFORMAT'];
     }
-    
-    
+
 }
 
 // replace and remove
@@ -57,4 +58,3 @@ function format_date($date, $timestamp = false) {
         return date($cfg['DEFAULT_DATEFORMAT'], strtotime($date));
     }
 }
-
